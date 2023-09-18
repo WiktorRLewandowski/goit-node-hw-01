684bed29346ef36389d79f44e8421a9c40bb028c
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-09-18 19:47:12 +0200

    working ver

--- contacts.js
+++ contacts.js
@@ -19,7 +19,7 @@ async function readFile() {
 async function writeFile(data) {
     try {
         await fs.writeFile(contactsPath, JSON.stringify(data, null, 2))
-        console.log("List has been updated")
+        console.log("List has been updated".green)
     }
     catch(error) {
         console.log("Error while trying to overwrite the file".red, error)
@@ -35,11 +35,6 @@ async function listContacts() {
     catch(error) {
         console.log("Error while listing the contacts:".red, error)
     }
-    // fs.readFile(contactsPath)
-    // .then(data => (
-    //     console.log('Contacts list'.cyan),
-    //     console.table(JSON.parse(data))))
-    // .catch(error => console.error('Error while listing contacts:', error.red))
 }
 
 async function getContactById(contactId) {
@@ -48,31 +43,26 @@ async function getContactById(contactId) {
     searchedContact = contacts.find(contact => contact.id === contactId)
     return searchedContact
         ? console.table(searchedContact)
-        : console.log('No such contact'.red)
+        : console.log('No such contact!'.red)
     }
     catch(error) {
-        console.log("Error while trying to get the contact by ID".red, error)
+        console.log("Error while trying to get the contact by ID!".red, error)
     }
-    // fs.readFile(contactsPath)
-    // .then(data => JSON.parse(data).find(contact = contact.id === contactId))
-    // .catch(error => console.log("Couldn't find contact with such ID".red, error))
 }
-
-// getContactById('AeHIrLTr6JkxGE6SN-0Rw')
 
 async function removeContact(contactId) {
     try {
         const contacts = await readFile()
         const filteredContacts = contacts.filter(contact => contact.id !== contactId)
-        if (filteredContacts.length < contacts) {
+        if (filteredContacts.length < contacts.length) {
+            await console.log(`Contact with ID:`.green, `${contactId}`,'removed.'.green)
             await writeFile(filteredContacts)
-            console.log(`Contact with ID:${contactID} removed`.green)
         } else {
             console.log(`Contact with ID:${contactId} not found!`.red)
         }
     }
     catch(error) {
-        console.log("Error while trying to remove the contact".red, error)
+        console.log("Error while trying to remove the contact!".red, error)
     }
 
 }
@@ -86,7 +76,15 @@ async function addContact(name, email, phone) {
         email,
         phone
         };
-        await writeFile([...contacts, newContact])
+        const filteredContact = contacts.filter(contact => contact.name === name) 
+        if (!filteredContact[0]) {
+            contacts.push(newContact)
+            await writeFile(contacts)
+            console.log(`${name} has been added to your contact list.`)
+        } else {
+            console.log(`The name:${name} is already used in database.`.yellow)
+        }
+        //why oh why spread operator leaves only first array index??
     }
     catch(error) {
         console.log('Error while trying to add a contact'.red, error)
